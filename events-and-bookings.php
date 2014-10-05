a2dc93bf009e16de1b1a4cd461f31d21c52dc1bb
vladislav <ve@incsub.com>
2014-10-05 08:28:55 +0200

    Updating the Beta version number (v1.7.6.2-BETA-3)

--- events-and-bookings.php
+++ events-and-bookings.php
@@ -6,7 +6,7 @@
  Author: WPMU DEV
  Text Domain: eab
  WDP ID: 249
- Version: 1.7.6.2-BETA-2
+ Version: 1.7.6.2-BETA-3
  Author URI: http://premium.wpmudev.org
 */
 
@@ -30,7 +30,7 @@ class Eab_EventsHub {
 	 * @TODO Update version number for new releases
      * @var	string
      */
-    const CURRENT_VERSION = '1.7.6.2-BETA-2';
+    const CURRENT_VERSION = '1.7.6.2-BETA-3';
     
     /**
      * Translation domain
