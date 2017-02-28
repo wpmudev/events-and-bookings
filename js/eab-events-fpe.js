9c007637b530b07fadd8fa46e937ae2c19ab6ddb
Ashok (Bappi D Great) <aknath.707@gmail.com>
2017-02-28 23:37:58 +0600

    Fix: Delete attendence was not working from front end

--- js/eab-events-fpe.js
+++ js/eab-events-fpe.js
@@ -209,7 +209,7 @@ $(function () {
 		toggle_fee();
 	}
 	
-        $("body").on("click", ".eab-add_attendance .button", function () {
+    $("body").on("click", ".eab-add_attendance .button", function () {
 		var $root = $(".eab-add_attendance"),
 			event_id = $root.find(".eab-attendance-event_id").val()
 			email = $root.find(".eab-attendance-email").val(),
@@ -223,6 +223,21 @@ $(function () {
 			status: status
 		}, function (data) {
 			$(".eab-add_attendance-container").html(data);
+		});
+		return false;
+	});
+    
+    // Attendance deleting
+	$("body").on("click", ".eab-guest-delete_attendance", function () {
+		var $me = $(this);
+		var user_id = $me.attr("data-eab-user_id");
+		var post_id = $me.attr("data-eab-event_id");
+		$.post(ajaxurl, {
+			"action": "eab_delete_attendance",
+			"user_id": user_id,
+			"post_id": post_id
+		}, function (data) {
+			$("#eab-bookings-response").html(data);
 		});
 		return false;
 	});
