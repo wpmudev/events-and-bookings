df43a04dcc24c02ae306dcc5d19904b956e8feab
Panos Lyrakis <panos.lyrakis@gmail.com>
2017-06-26 17:45:02 +0300

    Invalid Schema Markup Causes Validation Errors

--- lib/class_eab_template.php
+++ lib/class_eab_template.php
@@ -554,7 +554,10 @@ class Eab_Template {
 
 		if ($event->has_venue()) {
 			$venue = $event->get_venue_location(Eab_EventModel::VENUE_AS_ADDRESS);
-			$content .= "<div class='wpmudevevents-location' itemprop='location'>{$venue}</div>";
+			$content .= "<div class='wpmudevevents-location' itemprop='location' itemscope itemtype='http://schema.org/Place'>{$venue}
+                            <span itemprop='address' itemscope itemtype='http://schema.org/PostalAddress'>            
+                            </span>
+                        </div>";
 		}
 		if ($event->is_premium()) {
 			$price = $event->get_price();
