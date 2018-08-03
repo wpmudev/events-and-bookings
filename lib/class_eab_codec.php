27a4b3c9602cba21f075a8a32339b6c1f1123170
Unknown <vaughan@incsub.com>
2018-08-03 04:11:04 +0100

    added fix for relative_date
    Added fix for relatie_date caused by previous Default All Events fix.

--- lib/class_eab_codec.php
+++ lib/class_eab_codec.php
@@ -59,14 +59,15 @@ abstract class Eab_Codec {
 			$relative_date = strtotime($args["relative_date"], $pivot);
 			if ($relative_date) $args['date'] = date("Y-m-d H:i:s", $relative_date);
 		}
-		if (isset($raw['date'])) {
-                    $args['date'] = $this->_arg_to_time($args['date']);
-                } else {
-                    if(isset($raw['show_old']) && $this->_arg_to_bool($raw['show_old'])) {
-                        $args['date'] = $accepted['date'];
-                    }
-                }
 
+		if (isset($args['date'])) {
+                    $args['date'] = $this->_arg_to_time($args['date']);
+                } 
+		
+                if(isset($raw['show_old']) && $this->_arg_to_bool($raw['show_old'])) {
+                        $args['date'] = $accepted['date'];
+                }
+		
 		if (isset($raw['lookahead'])) $args['lookahead'] = $this->_arg_to_bool($args['lookahead']);
 		if (isset($raw['weeks'])) $args['weeks'] = $this->_arg_to_int($args['weeks']);
 
