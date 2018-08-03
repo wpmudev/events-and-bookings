93a8ae131a63694e0af16d6f4c22b6c523539138
Unknown <vaughan@incsub.com>
2018-08-03 23:50:51 +0100

    removed redundant code
    removed no longer necessary code.

--- lib/shortcodes/class-eab-archive-shortcode.php
+++ lib/shortcodes/class-eab-archive-shortcode.php
@@ -23,14 +23,6 @@ class Eab_Archive_Shortcode extends Eab_Codec {
 
 		$events = array();
 		
-		$day_only = $this->args['day_only']
-			? create_function( '', 'return ' . $this->args['day_only'] . ';' )
-			: false;
-		
-		if($day_only) {
-		    add_filter('eab-collection-upcoming-day_only', $day_only);
-		}
-		
 		if ( is_multisite() && $this->args['network'] ) {
 			$events = Eab_Network::get_archive_events( 30 );
 		} else {
